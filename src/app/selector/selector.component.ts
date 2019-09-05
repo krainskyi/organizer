f982d8656ba2a4c866f455e9504f5581859e2e90
krainskyi <androidlap@gmail.com>
2019-09-05 14:50:49 +0300

    05.09/14:50

---
+++ src/app/selector/selector.component.ts
@@ -0,0 +1,16 @@
+import { DateService } from './../shared/date.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selector',
+  templateUrl: './selector.component.html',
+  styleUrls: ['./selector.component.css']
+})
+export class SelectorComponent implements OnInit {
+
+  constructor(private dateService:DateService) { }
+
+  ngOnInit() {
+  }
+
+}
